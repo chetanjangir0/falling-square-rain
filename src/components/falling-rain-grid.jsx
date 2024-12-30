f99d7ba2e01227e09946073d35ede0309e660d29
Chetan <chetanrjj2002@gmail.com>
2024-12-30 14:39:45 +0530

    added some css for the grid

--- src/components/falling-rain-grid.jsx
+++ src/components/falling-rain-grid.jsx
@@ -3,12 +3,14 @@
 export default function FallingRainGrid({rows = 10, cols = 10}){
     const grid = Array(rows * cols).fill(null);
     return <>
-        <div className="grid">
-            {grid.map((_, i)=>(
-                <div key={i} className={`box${i}`}>
-                    {i + 1}
-                </div>
-            ))}
-        </div>
+        <section className="gridContainer">
+            <div className="grid">
+                {grid.map((_, i)=>(
+                    <div key={i} className={`box`}>
+                        {i + 1}
+                    </div>
+                ))}
+            </div>
+        </section>
     </>
 }
