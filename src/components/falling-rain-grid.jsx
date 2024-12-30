0dd8d45750c828568f1068aad78cec6502b0d989
Chetan <chetanrjj2002@gmail.com>
2024-12-30 22:31:42 +0530

    created the logic for rainfall

--- src/components/falling-rain-grid.jsx
+++ src/components/falling-rain-grid.jsx
@@ -1,23 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+
 
 
 export default function FallingRainGrid({rows = 10, cols = 10}){
-    const [grid, setGrid] = useState(Array(rows * cols).fill("black")); 
+    const [raindrops, setRaindrops] = useState([]);
 
-    const handleClick = (i) => {
-        const updatedGrid = [...grid];
-        updatedGrid[i] = "blue";
-        setGrid(updatedGrid);
-    };
+    useEffect(()=>{
+        const interval = setInterval(() => {
+            // update raindrops position
+            setRaindrops(prevDrops => {
+                const updatedDrops = prevDrops.map(drop => 
+                    drop.row + 1 < rows ? {...drop, row:drop.row + 1} : drop
+                );
+
+                // add a new drop optionally
+                if(Math.random() < 0.5){
+                    const newDrop = {col:Math.floor(Math.random() * cols), row:0}
+                    updatedDrops.push(newDrop);
+                }
+                return updatedDrops;
+            });
+            console.log(raindrops)
+        }, 200);
+
+        return () => clearInterval(interval);
+    }, [cols, rows])
+
+
+    const hasRaindrop = (index) =>{
+        const r = Math.floor(index / cols);
+        const c = index % cols;
+        return raindrops.some(drop => drop.row == r && drop.col == c);
+    }
+
+
     return <>
         <section className="gridContainer">
             <div className="grid" style={{"--rows":rows, "--cols":cols}}>
-                {grid.map((_, i)=>(
+                {Array(rows * cols).fill().map((_, i)=>(
                     <div 
                         key={i} 
-                        className={`box`} 
-                        style={{backgroundColor: grid[i]}}
-                        onClick={()=>handleClick(i)}
+                        className={`box`}
+                        style={{backgroundColor:hasRaindrop(i) ? "blue" : ''}}
                     >
                         {i + 1}
                     </div>
