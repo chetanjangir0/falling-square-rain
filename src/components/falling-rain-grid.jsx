614b4e66a1c97d10e379de8f2f1a421073a7551c
Chetan <chetanrjj2002@gmail.com>
2024-12-30 22:59:19 +0530

    fixed the drops being stuck at last row

--- src/components/falling-rain-grid.jsx
+++ src/components/falling-rain-grid.jsx
@@ -1,20 +1,20 @@
 import { useEffect, useState } from "react";
 
-
-
-
 export default function FallingRainGrid({rows = 10, cols = 10}){
     const [raindrops, setRaindrops] = useState([]);
 
     useEffect(()=>{
         const interval = setInterval(() => {
-            // update raindrops position
+
             setRaindrops(prevDrops => {
-                const updatedDrops = prevDrops.map(drop => 
-                    drop.row + 1 < rows ? {...drop, row:drop.row + 1} : drop
+                // update raindrops position
+                const updatedDrops = prevDrops
+                    .filter(drop => drop.row < rows)
+                    .map(drop => ({...drop, row:drop.row + 1})
                 );
 
-                // add a new drop optionally
+
+                // add a new drop randomly
                 if(Math.random() < 0.5){
                     const newDrop = {col:Math.floor(Math.random() * cols), row:0}
                     updatedDrops.push(newDrop);
@@ -22,7 +22,7 @@ export default function FallingRainGrid({rows = 10, cols = 10}){
                 return updatedDrops;
             });
             console.log(raindrops)
-        }, 200);
+        }, 100);
 
         return () => clearInterval(interval);
     }, [cols, rows])
