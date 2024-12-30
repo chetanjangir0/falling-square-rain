d300c33c228edae329de2c945e8b15f178d88885
Chetan <chetanrjj2002@gmail.com>
2024-12-30 16:04:30 +0530

    added some color logic and fixed some css

--- src/components/falling-rain-grid.jsx
+++ src/components/falling-rain-grid.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 
 
 export default function FallingRainGrid({rows = 10, cols = 10}){
-    const grid = Array(rows * cols).fill(null);
+    const [grid, setGrid] = useState(Array(rows * cols).fill("black")); 
+
+    const handleClick = (i) => {
+        const updatedGrid = [...grid];
+        updatedGrid[i] = "blue";
+        setGrid(updatedGrid);
+    };
     return <>
         <section className="gridContainer">
-            <div className="grid">
+            <div className="grid" style={{"--rows":rows, "--cols":cols}}>
                 {grid.map((_, i)=>(
-                    <div key={i} className={`box`}>
+                    <div 
+                        key={i} 
+                        className={`box`} 
+                        style={{backgroundColor: grid[i]}}
+                        onClick={()=>handleClick(i)}
+                    >
                         {i + 1}
                     </div>
                 ))}
