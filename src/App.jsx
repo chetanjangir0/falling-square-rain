f202e3a37eaa7f25a9c50546304520d8cdb4604e
Chetan <chetanrjj2002@gmail.com>
2025-01-03 22:49:17 +0530

    added sliders to change grid size

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 import FallingRainGrid from './components/falling-rain-grid';
 
 function App() {
-
+  const [gridSize, setGridSize] = useState({rows:15, cols:20});
   return (
     <>
       <h1>Falling Rain</h1>
-      <FallingRainGrid rows={15} cols = {20}/>
+      <div>
+        <input
+          type="range"
+          min="5"
+          max="20"
+          value={gridSize.rows}
+          onChange={(e) => setGridSize((prev) => ({ ...prev, rows: +e.target.value }))} // + converts the string to numbers
+        />
+        <input
+          type="range"
+          min="5"
+          max="20"
+          value={gridSize.cols}
+          onChange={(e) => setGridSize((prev) => ({ ...prev, cols: +e.target.value }))}
+        />
+      </div>
+      <FallingRainGrid rows={gridSize.rows} cols = {gridSize.cols}/>
+
     </>
   )
 }
